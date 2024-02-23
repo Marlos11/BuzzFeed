ead5dd0715c80eea790f773a8e97f373b97fff03
Marlos11 <marlos-gama@hotmail.com>
2024-02-23 08:50:39 -0300

    created class and component types

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+
+
+
+export class QuizzComponent implements OnInit {
+
+  title: string = ""
+  question: any
+  questionSelected: any
+
+  answers: string[] = []
+  answersSelected: string = ""
+
+  qustionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  finished: boolean = false
+
+  ngOnInit(): void { }
 
 }
